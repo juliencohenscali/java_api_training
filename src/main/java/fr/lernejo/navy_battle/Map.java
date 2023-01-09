d29246010b1b259f7ce00cb44bdbfeda4a5af29c
julien.cohen-scali <julien.cohen-scali@epita.fr>
2023-01-09 19:04:10 +0100

    Update errors and recommendation

--- src/main/java/fr/lernejo/navy_battle/Map.java
+++ src/main/java/fr/lernejo/navy_battle/Map.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Map {
 
-    public final ToolMethod toolMethod = new ToolMethod();
+    private final ToolMethod toolMethod = new ToolMethod();
 
     public Map() throws InterruptedException {
     }
--- src/main/java/fr/lernejo/navy_battle/Server.java
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -20,56 +20,16 @@ class Server{
         List<String> shotCase = new ArrayList<>();
         List<Object> clientGlobalMap = clientMap.flushMap();
         List<Object> serverGlobalMap = serverMap.flushMap();
-
-
         ToolMethod toolMethod = new ToolMethod();
         String id = toolMethod.genId();
         InetSocketAddress address = new InetSocketAddress("localhost", Integer.parseInt(args[0]));
-        HttpServer httpServer = HttpServer.create(address, 1);
-        ExecutorService executor = Executors.newFixedThreadPool(1);
-        httpServer.setExecutor(executor);
-        System.out.println("Starting on port " + args[0] +"...");//Http Server Config
-
-        HttpHandler httpHandlerPing = httpExchange -> { Headers head = httpExchange.getRequestHeaders();        //Ping context declaration
-            System.out.println("[*] Ping From:" + head.get("Host"));
-            String response = "ok\n";httpExchange.sendResponseHeaders(200, response.getBytes().length);
-            httpExchange.getResponseBody().write(response.getBytes());
-            httpExchange.close();
-        };
-
-        HttpHandler httpHandlerApiGameStart = httpExchange -> {
-            if (Objects.equals(httpExchange.getRequestMethod(), "POST")) {            //Context in case of POST request
-                System.out.println(new String(httpExchange.getRequestBody().readAllBytes()));
-                System.out.println("[*] ApiGameStart " + httpExchange.getRequestMethod() + " From:" + httpExchange.getRemoteAddress().getAddress() + ':' + httpExchange.getRemoteAddress().getPort());
-                System.out.println();
-
-                serverMap.flushMap();
-                clientMap.flushMap();
-                shotCase.clear();
-
-                String res = "{\n    \"id\": \"" + id + "\",\n    \"url\": \"http://localhost:" + args[0] + "\",\n    \"message\": \"May the best code win\"\n}";
-                httpExchange.getResponseHeaders().add("From","server");
-                httpExchange.sendResponseHeaders(202, res.getBytes().length);
-                httpExchange.getResponseBody().write(res.getBytes());
-                try (OutputStream os = httpExchange.getResponseBody()) { // (1)
-                    os.write(res.getBytes());
-                }                httpExchange.close();
-            }
-            else{//Context in case of other Method request
-                Headers head = httpExchange.getRequestHeaders();
-                System.out.println("[!] GET request to ApiGameStart From:" + head.get("Host"));
-                String res = "this context only accepts POST request\n";
-                httpExchange.sendResponseHeaders(404, res.getBytes().length);
-                httpExchange.getResponseBody().write(res.getBytes());
-                httpExchange.close();
-            }
-        };// Start context declaration
+        HttpServer httpServer = HttpServer.create(address, 1);ExecutorService executor = Executors.newFixedThreadPool(1);httpServer.setExecutor(executor);
+        System.out.println("Starting on port " + args[0] +"...");
+        HttpHandler httpHandlerPing = httpExchange -> { Headers head = httpExchange.getRequestHeaders(); System.out.println("[*] Ping From:" + head.get("Host"));String response = "OK";httpExchange.sendResponseHeaders(200, response.getBytes().length);httpExchange.getResponseBody().write(response.getBytes());httpExchange.close();};
+        HttpHandler httpHandlerApiGameStart = httpExchange -> { if (Objects.equals(httpExchange.getRequestMethod(), "POST")) {            System.out.println(new String(httpExchange.getRequestBody().readAllBytes()));System.out.println("[*] ApiGameStart " + httpExchange.getRequestMethod() + " From:" + httpExchange.getRemoteAddress().getAddress() + ':' + httpExchange.getRemoteAddress().getPort());System.out.println();serverMap.flushMap();clientMap.flushMap();shotCase.clear();String res = "{\n    \"id\": \"" + id + "\",\n    \"url\": \"http://localhost:" + args[0] + "\",\n    \"message\": \"May the best code win\"\n}";httpExchange.getResponseHeaders().add("From","server");httpExchange.sendResponseHeaders(202, res.getBytes().length);httpExchange.getResponseBody().write(res.getBytes());try (OutputStream os = httpExchange.getResponseBody()) {     os.write(res.getBytes());}httpExchange.close();            }            else{ Headers head = httpExchange.getRequestHeaders();System.out.println("[!] GET request to ApiGameStart From:" + head.get("Host"));String res = "this context only accepts POST request\n";httpExchange.sendResponseHeaders(404, res.getBytes().length);httpExchange.getResponseBody().write(res.getBytes());httpExchange.close();            }        };
         GiantHTTPContextDeclaration stupidClass = new GiantHTTPContextDeclaration();
         HttpHandler httpHandlerApiGameFire = stupidClass.StupidGiantHTTPContextDeclaration(serverMap, clientMap, toolMethod, this, shotCase, (List<List<String>>) serverGlobalMap.get(1), (char[][]) serverGlobalMap.get(0), (List<List<String>>) clientGlobalMap.get(1), (char[][]) clientGlobalMap.get(0));;
-        httpServer.createContext("/ping", httpHandlerPing);
-        httpServer.createContext("/api/game/start", httpHandlerApiGameStart);
-        httpServer.createContext("/api/game/fire", httpHandlerApiGameFire);
-        httpServer.start();
+        httpServer.createContext("/ping", httpHandlerPing); httpServer.createContext("/api/game/start", httpHandlerApiGameStart); httpServer.createContext("/api/game/fire", httpHandlerApiGameFire); httpServer.start();
     }
     public String createFireResponse(String result, String boatLeft){
         return "{\n    \"consequence\": \"" + result + "\",\n    \"shipLeft\": " + boatLeft +"\n}";
